38ad4efa1e7f8477d57bbf6d9c7ef6f08d664cf0
Imran Ariffin <ariffin.imran@gmail.com>
2023-07-16 15:16:25 -0400

    Use blacklist file, configurable parameters, and do retry on connection error (#1)
    * Configurable parameters & retry on connection error
    * Update README
    * Stagger download randomly
    * Cleanup: move private funcs to bottom

--- README.md
+++ README.md
@@ -1,14 +1,56 @@
+# kehadiran-scraper
+
+Scrape the www.parlimen.gov.my website to download the hansard pdf files.
+
 ## Development
 
 Make sure that Python is installed correctly.
 
-    python3 --version
-    python3 -m pip --version
+```bash
+python3 --version
+python3 -m pip --version
+```
 
 Install required packages.
 
-    python3 -m pip install -r requirements.txt
+```bash
+python3 -m pip install -r requirements.txt
+```
+Run
 
-Run the program.
+```bash
+python kehadiran_scraper/main.py
+```
 
-    python3 kehadiran_scraper/main.py
+If you want, you can also specify these environment variables to run with specific parameters:
+
+```bash
+LOG_LEVEL=INFO \
+SCRAPE_CHUNK_SIZE=15 \
+SLEEP_S=0.25 \
+MIN_DATE=2015-01-01 \
+MAX_DATE=2023-07-09 \
+DISCONNECT_BACKOFF_S=5 \
+OUTPUT_DIR=some_output_dir \
+python kehadiran_scraper/main.py
+
+# LOG_LEVEL: Set the log level (DEBUG, INFO, WARNING, ERROR)
+#
+# SLEEP_S: Set the time in seconds to sleep before each chunk
+#
+# MIN_DATE: Set the min date to scrape. We start scraping
+# from MAX_DATE down to MIN_DATE (latest to earliest)
+#
+# MAX_DATE: Set the max date to scrape. We start scraping
+# from MAX_DATE down to MIN_DATE (latest to earliest)
+#
+# DISCONNECT_BACKOFF_S: Set the time in seconds to backoff and wait before
+# trying again  after getting a "Server disconnected" error. On each next try,
+# we try again from the last date that failed so we're not going
+# to do redundant work
+#
+# OUTPUT_DIR: Directory to save the output pdf files. Thedirectory will
+# be automatically created if it doesn't exist.
+```
+
+The hansard pdf files can found out at `output/` directory. The blacklisted dates will be generated at `.blacklist` file and the file will be populated as the script is running, so that if you run the script again with the same dates, it will run faster. If you ran the script and exited successful, and found out that the files in `output/` have changed, please create a branch, commit these two files and file a PR. This might mean that they have uploaded new or updated files.
--- kehadiran_scraper/main.py
+++ kehadiran_scraper/main.py
@@ -1,93 +1,217 @@
 import asyncio
 import datetime as dt
 import logging
-import os
+from os import environ
+from random import random
 import typing as t
+from xmlrpc.client import TRANSPORT_ERROR
 
 import aiofiles
+from aiofiles import os, ospath
 import aiohttp
 
-# TODO: Read from env var for some of these
-SCRAPE_CHUNK_SIZE = 50
-SLEEP_S = 1
-FIRST_DATE = dt.date(1959, 9, 11)
+SCRAPE_CHUNK_SIZE = int(environ.get("SCRAPE_CHUNK_SIZE", 5))
+SLEEP_S = float(environ.get("SLEEP_S", 0.5))
+DISCONNECT_BACKOFF_S = float(environ.get("DISCONNECT_BACKOFF_S", 10))
+MIN_DATE = dt.datetime.strptime(
+    environ.get("MIN_DATE", "1959-09-11"), "%Y-%m-%d"
+).date()
+MAX_DATE_DEFAULT = dt.datetime.strftime(dt.datetime.today(), "%Y-%m-%d")
+MAX_DATE = dt.datetime.strptime(
+    environ.get("MAX_DATE", MAX_DATE_DEFAULT), "%Y-%m-%d"
+).date()
 URL_PATTERN = "https://www.parlimen.gov.my/files/hindex/pdf/DR-{day}{month}{year}.pdf"
-TEST_OUTPUT_DIR = ".test.out/"
+OUTPUT_DIR = environ.get("OUTPUT_DIR", "output/")
+BLACKLIST_FILENAME = ".blacklist"
+LOG_LEVEL = environ.get("LOG_LEVEL", "DEBUG")
 
 T = t.TypeVar("T")
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("kehadiran_scraper.main")
 
 
-def _chunker(it: t.Iterator[T], size: int) -> t.Iterator[tuple[T]]:
-    chunk = []
-    for item in it:
-        chunk.append(item)
-        if len(chunk) == size:
-            yield tuple(chunk)
-            chunk = []
-    if chunk:
-        yield tuple(chunk)
+def main():
+    async def _keep_retrying():
+        visited_dates = []
+        min_date = MIN_DATE
+        max_date = MAX_DATE
+
+        while True:
+            try:
+                await scrape(
+                    min_date=min_date,
+                    max_date=max_date,
+                    visited_dates=visited_dates,
+                )
+            except aiohttp.ServerConnectionError:
+                logger.warning(
+                    "Server disconnected error. Trying again after %s seconds",
+                    DISCONNECT_BACKOFF_S,
+                )
+                await asyncio.sleep(DISCONNECT_BACKOFF_S)
+                max_date = min(visited_dates)
+            else:
+                logger.info("All dates have been scraped. Exiting with success.")
+                return
+
+    asyncio.run(_keep_retrying())
 
 
-def _url_generator(start: dt.date) -> t.Iterator[str]:
-    today = dt.date.today()
-    days = (today - start).days
-    for i in range(days):
-        cur_date: dt.date = start + dt.timedelta(days=i)
-        url: str = URL_PATTERN.format(
-            day=str(cur_date.day).zfill(2),
-            month=str(cur_date.month).zfill(2),
-            year=cur_date.year,
+async def scrape(min_date: dt.date, max_date: dt.date, visited_dates: list[dt.date]):
+    blacklist: set[dt.date] = await _get_blacklist_dates_from_file(
+        create_if_not_exist=True
+    )
+
+    async with aiohttp.ClientSession() as session:
+        urls_iter: t.Iterator[tuple[str, dt.date]] = _url_generator(
+            min_date=min_date,
+            max_date=max_date,
+            blacklist=blacklist,
         )
-        yield url
+
+        for urls in _chunker(urls_iter, size=SCRAPE_CHUNK_SIZE):
+            url_to_date: dict[str, dt.date] = {}
+            download_tasks = []
+            for i, (url, cur_date) in enumerate(urls):
+                if i != 0:
+                    random_delay_s: float = random() * SLEEP_S
+                    task = download(url=url, session=session)
+                    task = _stagger(task, t_s=random_delay_s)
+                else:
+                    task = download(url=url, session=session)
+                download_tasks.append(task)
+                url_to_date[url] = cur_date
+
+            logger.info(
+                "Downloading files from %s urls, from %s to %s",
+                *(
+                    len(download_tasks),
+                    url_to_date[urls[0][0]],
+                    url_to_date[urls[-1][0]],
+                ),
+            )
+            files = await asyncio.gather(*download_tasks)
+
+            save_tasks = []
+            blacklist_new = set()
+            for data, url in files:
+                cur_date = url_to_date[url]
+                if data is None:
+                    blacklist_new.add(cur_date)
+                    continue
+                save_tasks.append(save(data=data, date=cur_date))
+            blacklist.update(blacklist_new)
+
+            logger.info(
+                "Saving %s downloaded files & adding %s dates to blacklist",
+                *(len(save_tasks), len(blacklist_new)),
+            )
+            await asyncio.gather(
+                *save_tasks,
+                _update_blacklist_to_file(blacklist=blacklist),
+            )
+            for cur_date in url_to_date.values():
+                visited_dates.append(cur_date)
 
 
 async def download(
     url: str,
     session: aiohttp.ClientSession,
 ) -> tuple[t.Optional[bytes], t.Optional[str]]:
-    logger.info("Downloading from %s", url)
     async with session.get(url) as response:
         data: bytes = await response.read()
         if b"File does not exist" in data:
-            # TODO: Collect url in a blacklist set. Later we can dump into file
-            logger.error("Url %s does not contain file. Need to blacklist", url)
-            return (None, None)
+            return (None, url)
         return data, url
 
 
-async def save(data: bytes, url: str):
-    # TODO: Write to S3
-    filename: str = TEST_OUTPUT_DIR + url.split("/")[-1]
-    logger.info("Found file from %s, dumping to file %s", url, filename)
+async def save(data: bytes, date: dt.date):
+
+    async def _mkdir_if_not_exist(dirname: str) -> bool:
+        try:
+            await os.stat(dirname)
+            await os.mkdir(OUTPUT_DIR)
+            return True
+        except (ValueError, OSError):
+            return False
+
+    async with asyncio.Lock():
+        await _mkdir_if_not_exist(OUTPUT_DIR)
+
+    filename: str = ospath.path.join(
+        OUTPUT_DIR,
+        dt.date.strftime(date, "%Y-%m-%d") + ".pdf",
+    )
     async with aiofiles.open(filename, mode="wb") as fo:
         await fo.write(data)
 
 
-async def scrape():
-    async with aiohttp.ClientSession() as session:
-        # TODO: Skip url found in blacklist file (Or use whitelist instead)
-        for urls in _chunker(_url_generator(start=FIRST_DATE), size=SCRAPE_CHUNK_SIZE):
-            download_tasks = [download(url=url, session=session) for url in urls]
-            files = await asyncio.gather(*download_tasks)
-
-            save_tasks = [
-                save(data=data, url=url)
-                for data, url in files
-                if data is not None and url is not None
-            ]
-            await asyncio.gather(*save_tasks)
-            await asyncio.sleep(SLEEP_S)
+def _chunker(it: t.Iterator[T], size: int) -> t.Iterator[list[T]]:
+    chunk = []
+    for item in it:
+        chunk.append(item)
+        if len(chunk) == size:
+            yield chunk
+            chunk = []
+    if chunk:
+        yield chunk
 
 
-def main():
-    os.makedirs(TEST_OUTPUT_DIR, exist_ok=True)
-    asyncio.run(scrape())
+def _url_generator(
+    min_date: dt.date,
+    max_date: dt.date,
+    blacklist: set[dt.date],
+) -> t.Iterator[tuple[str, dt.date]]:
+    days = (max_date - min_date).days
+    for i in range(days):
+        cur_date: dt.date = max_date - dt.timedelta(days=i)
+        if cur_date in blacklist:
+            logger.debug("%s is blacklisted, skipping", cur_date)
+            continue
+        url: str = URL_PATTERN.format(
+            day=str(cur_date.day).zfill(2),
+            month=str(cur_date.month).zfill(2),
+            year=cur_date.year,
+        )
+        yield url, cur_date
+
+
+async def _get_blacklist_dates_from_file(
+    create_if_not_exist: bool = False,
+) -> set[dt.date]:
+    s: set[dt.date] = set()
+    if create_if_not_exist:
+        if not await ospath.exists(BLACKLIST_FILENAME):
+            # Open and close immediately to create new empty file
+            f = await aiofiles.open(BLACKLIST_FILENAME, mode="w")
+            await f.close()
+
+    async with aiofiles.open(BLACKLIST_FILENAME, mode="r") as fi:
+        async for line in fi:
+            line = line.strip("\n")
+            date = dt.date.fromisoformat(line.strip("\n"))
+            s.add(date)
+    return s
+
+
+async def _update_blacklist_to_file(blacklist: set[dt.date]) -> None:
+    blacklist_existing: set[dt.date] = await _get_blacklist_dates_from_file()
+    blacklist = blacklist - blacklist_existing
+    async with aiofiles.open(BLACKLIST_FILENAME, mode="a") as fo:
+        blacklist_sorted = "\n".join(sorted((str(d) for d in blacklist), reverse=True))
+        if blacklist_existing and blacklist_sorted:
+            blacklist_sorted = "\n" + blacklist_sorted
+        await fo.write(blacklist_sorted)
+
+
+async def _stagger(task: t.Awaitable[T], t_s: float) -> T:
+    await asyncio.sleep(t_s)
+    ret: T = await task
+    return ret
 
 
 if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=getattr(logging, LOG_LEVEL),
     )
     main()
